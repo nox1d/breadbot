c8452d1e7537ec88aa92c55f909e54ddca54c7ec
nox1d <114980767+nox1d@users.noreply.github.com>
2024-07-29 15:06:35 +0800

    Set new bot activity status

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,10 @@
-const { Events } = require("discord.js");
+const { Events, ActivityType } = require("discord.js");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
     console.log(`${client.user.tag} is online!`);
+    client.user.setActivity('you :3', { type: ActivityType.Listening });
   },
 };
